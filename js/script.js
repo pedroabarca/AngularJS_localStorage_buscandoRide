2746312846484168c4722f1b58e54dae8e5ed541
Pedro Abarca Méndez <tonyp3dro@gmail.com>
2016-07-01 00:57:04 -0600

    Portada Lista

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-(funtion(){
-  
- 	var salida = document.formulario.salida.value;
- 	var destino = document.formulario.destino.value;
 
+var saludo = function(){
 
+	var salida = document.createTextNode("zarcero");
+	var destino = document.createTextNode("sucre");
+    document.getElementById("salida1").appendChild(salida);
+    document.getElementById("salida2").appendChild(salida);
+    document.getElementById("salida3").appendChild(salida);
 
+};
+document.getElementById("add").addEventListener("click", saludo);
 
-
-}
-
-	)

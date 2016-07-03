2e79c28db4b3cb283fb156b9c360404754aac0a5
Pedro Abarca Méndez <tonyp3dro@gmail.com>
2016-07-02 22:19:04 -0600

    Avance: login,registr,dash,migas...

--- js/script.js
+++ js/script.js
@@ -3,10 +3,31 @@ var saludo = function(){
 
 	var salida = document.createTextNode("zarcero");
 	var destino = document.createTextNode("sucre");
-    document.getElementById("salida1").appendChild(salida);
-    document.getElementById("salida2").appendChild(salida);
-    document.getElementById("salida3").appendChild(salida);
+//    alert( $('#salida').val());
+
+    $('#salida1').html(  $('#salida').val());
+
+    $('#salida2').html(  $('#salida').val());
+
+    $('#salida3').html(  $('#salida').val());
+
+    $('#destino1').html(  $('#destino').val());
+
+    $('#destino2').html(  $('#destino').val());
+
+    $('#destino3').html(  $('#destino').val());
+
+
+
+    /*document.getElementById("#salida1").innerhtml = $('#salida').value;
+    document.getElementById("salida2").innerhtml = $('#salida').value;
+    document.getElementById("salida3").innerhtml = $('#salida').value;
+*/
 
 };
 document.getElementById("add").addEventListener("click", saludo);
 
+$('#myTabs a').click(function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})

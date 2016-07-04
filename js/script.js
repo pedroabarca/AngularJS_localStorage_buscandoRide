9836d20b773be2bd2f5480cbd33e0877f9a851ef
Pedro Abarca Méndez <tonyp3dro@gmail.com>
2016-07-04 00:40:35 -0600

    Dash: pills

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 
 var saludo = function(){
 
-	var salida = document.createTextNode("zarcero");
-	var destino = document.createTextNode("sucre");
-//    alert( $('#salida').val());
-
     $('#salida1').html(  $('#salida').val());
 
     $('#salida2').html(  $('#salida').val());
@@ -16,18 +12,10 @@ var saludo = function(){
     $('#destino2').html(  $('#destino').val());
 
     $('#destino3').html(  $('#destino').val());
-
-
-
-    /*document.getElementById("#salida1").innerhtml = $('#salida').value;
-    document.getElementById("salida2").innerhtml = $('#salida').value;
-    document.getElementById("salida3").innerhtml = $('#salida').value;
-*/
-
 };
 document.getElementById("add").addEventListener("click", saludo);
 
 $('#myTabs a').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
-})
+});

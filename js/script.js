de083238b39e2dc956889d185cf9f5f7d843f29e
Pedro Abarca Méndez <tonyp3dro@gmail.com>
2016-07-05 18:45:55 -0600

    Detalles de Rides y Configuración lista

--- js/script.js
+++ js/script.js
@@ -12,6 +12,22 @@ var saludo = function(){
     $('#destino2').html(  $('#destino').val());
 
     $('#destino3').html(  $('#destino').val());
+
+    $('#td1').html('Carlitos');
+
+    $('#td2').html('Julio');
+
+    $('#td3').html('Rodolfo');
+
+		
+    $('#ver1').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>');
+
+    $('#ver2').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>' );
+
+    $('#ver3').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>' );
+
+
+
 };
 document.getElementById("add").addEventListener("click", saludo);
 

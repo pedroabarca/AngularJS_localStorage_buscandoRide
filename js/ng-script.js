19eaaa2648ced49972b7f206cfaaf350c55fb69b
Pedro Abarca Méndez <tonyp3dro@gmail.com>
2016-08-21 22:47:25 -0600

    Registro con LocalStorage

---
+++ js/ng-script.js
@@ -0,0 +1,52 @@
+angular.module("Datos",["LocalStorageModule"])
+    .controller("DatosController",function ($scope,localStorageService) {
+
+
+        if(localStorageService.get("local.usuarios")){
+            $scope.usuarios =  localStorageService.get("local.usuarios");
+        }else{
+            $scope.usuarios = [];
+        }
+        if(localStorageService.get("local.rides")){
+            $scope.usuarios =  localStorageService.get("local.rides");
+        }else{
+            $scope.rides = [];
+        }
+
+        $scope.$watchCollection("usuarios",function (nuevValor,viejoValor) {
+            localStorageService.set("local.usuarios",$scope.usuarios);
+        });
+
+
+
+        $scope.validar = function () {
+            //verifica si existe numero o usuario
+            for (i = 0; i < $scope.usuarios.length; i++) {
+                if ($scope.usuarioNuevo.telefono == $scope.usuarios[i].telefono) {
+                    //el numero ya existe
+                    console.log("existe numero");
+                    $scope.usuarioNuevo = [];
+                    return false;
+                }
+                if ($scope.usuarioNuevo.usuario == $scope.usuarios[i].usuario) {
+                    // el nombre de usuario ya existe
+                    console.log("existe usuario");
+                    $scope.usuarioNuevo = [];
+                    return false;
+                }
+            }
+            //verifica las contraseñas
+            if ($scope.usuarioNuevo.contrasena == $scope.contra) {
+                $scope.usuarios.push($scope.usuarioNuevo);
+                $scope.usuarioNuevo = {};
+                console.log("registrado");
+                //registrado abre login
+
+            } else {
+                //las contraseñas no son la misma
+                console.log("contraseas diferents");
+            }
+        }
+
+
+    });//Controller
--- js/script.js
+++ js/script.js
@@ -1,33 +1,18 @@
 
-var saludo = function(){
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
 
-    $('#salida1').html(  $('#salida').val());
-
-    $('#salida2').html(  $('#salida').val());
-
-    $('#salida3').html(  $('#salida').val());
-
-    $('#destino1').html(  $('#destino').val());
-
-    $('#destino2').html(  $('#destino').val());
-
-    $('#destino3').html(  $('#destino').val());
-
-    $('#td1').html('Carlitos');
-
-    $('#td2').html('Julio');
-
-    $('#td3').html('Rodolfo');
-
-		
-    $('#ver1').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>');
-
-    $('#ver2').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>' );
-
-    $('#ver3').html('<a href="https://www.google.es/maps/place/Costa+Rica">Ver</a>' );
+//if (typeof(Storage) !== "undefined") {
+    // Code for localStorage/sessionStorage.
+  //  Storage.prototype.setObj = function(key, obj) {
+    //    return this.setItem(key, JSON.stringify(obj))
+    //}
+    //Storage.prototype.getObj = function(key) {
+      //  return JSON.parse(this.getItem(key))
+    //}
 
 
 
-};
-document.getElementById("add").addEventListener("click", saludo);
 
